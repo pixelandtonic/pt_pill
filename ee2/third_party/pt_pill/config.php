5456eb76e668fba0fd0d25b8ee6ddd0ace453030
Brandon Kelly <me@brandon-kelly.com>
2010-08-26 09:46:17 -0700

    1.0.2

--- ee2/third_party/pt_pill/config.php
+++ ee2/third_party/pt_pill/config.php
@@ -1,5 +1,5 @@
 <?php
 
 $config['name']    = 'P&amp;T Pill';
-$config['version'] = '1.0.1';
+$config['version'] = '1.0.2.1';
 $config['nsm_addon_updater']['versions_xml'] = 'http://pixelandtonic.com/ee/releasenotes.rss/pt_pill';
